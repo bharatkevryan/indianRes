04877e661af95df5d8cef64d8b534348496bf1f8
unknown <bharatdance2@gamil.com>
2020-09-20 11:45:48 +0530

    Updates

---
+++ precache-manifest.cd17182b71f19c85a1ac7255681c0656.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841930d59a32e1750a394d1ebb6339b0",
+    "url": "/indianRes/index.html"
+  },
+  {
+    "revision": "1c60f0aaa5560c2f945d",
+    "url": "/indianRes/static/css/main.1433f2f7.chunk.css"
+  },
+  {
+    "revision": "f45788a5097adb8aaab8",
+    "url": "/indianRes/static/js/2.6f4d11a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/indianRes/static/js/2.6f4d11a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c60f0aaa5560c2f945d",
+    "url": "/indianRes/static/js/main.7ef6a588.chunk.js"
+  },
+  {
+    "revision": "560198a2718524575f0b",
+    "url": "/indianRes/static/js/runtime-main.364ec7cb.js"
+  },
+  {
+    "revision": "3aa3603b1ef5b6257bad9220eefe61bb",
+    "url": "/indianRes/static/media/food.3aa3603b.png"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/indianRes/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/indianRes/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/indianRes/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/indianRes/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/indianRes/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/indianRes/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4ccf6b4457e93a909a2d0cff65ca177b",
+    "url": "/indianRes/static/media/menu.4ccf6b44.png"
+  },
+  {
+    "revision": "0e0f30b642cd66f07c9d20654cafc486",
+    "url": "/indianRes/static/media/rest1200.0e0f30b6.jpg"
+  },
+  {
+    "revision": "50593522c232a39381f6c0a1e000ddec",
+    "url": "/indianRes/static/media/rest768.50593522.jpg"
+  },
+  {
+    "revision": "12ceca8b778ae4be69511f38046befcf",
+    "url": "/indianRes/static/media/rest992.12ceca8b.jpg"
+  }
+]);
